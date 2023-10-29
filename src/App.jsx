dd3a43ae91a20181e2f0d568bd7bfc3d355a849d
Chaitanya Sai Chandu Yendru <chaitanyayendru001@gmail.com>
2023-10-29 09:16:38 -0400

    Added setup_import libraries --refs CS-001

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+
+import {About, Contact, Experience,Feedbacks,Hero, Navbar,Tech,Works,StarCanvas} from './'
+const App = () => {
+
+  return (
+    <div>
+      3D Developer portfolio
+    </div>
+  )
+}
+
+export default App
